3544c7ca2e4314ce3d2ab2c0dc9cf21b1cbf0a76
Calarence <749826557@qq.com>
2016-11-26 08:32:10 +0800

    刷新

--- pages/activityPost/activityPost.js
+++ pages/activityPost/activityPost.js
@@ -8,7 +8,7 @@ Page({
     lines: [
       { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }, { id: 9 }, { id: 10 }, { id: 11 }
     ],
-    names: ["骑行", "桌游","游泳", "足球","DOAT"],
+    names: ["骑行", "桌游", "游泳", "足球", "DOAT"],
     array: [{ id: 21, name: '骑行' }, { id: 22, name: "桌游" }, { id: 23, name: "游泳" }, {
       id: 24, name: "足球"
     }, { id: 25, name: "DOAT" }
@@ -22,14 +22,14 @@ Page({
       activityname: this.data.names[e.detail.value]
     })
   },
-  getActivityId: function(name){
-    for(var i=0,l=this.data.array.length;i<l;i++){
-for(var key in this.data.array[i]){
-   if(this.data.array[i][key]===name){
-     return this.data.array[i].id
-   }
-}
-}
+  getActivityId: function (name) {
+    for (var i = 0, l = this.data.array.length; i < l; i++) {
+      for (var key in this.data.array[i]) {
+        if (this.data.array[i][key] === name) {
+          return this.data.array[i].id
+        }
+      }
+    }
   },
   //提交活动内容
   formSubmit: function (e) {
@@ -42,38 +42,38 @@ for(var key in this.data.array[i]){
     })
     console.log(id)
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
-   wx.request({
-  url: 'http://172.21.101.175:11000/uplus/activity',
-  data: util.json2Form({
-    userId: "1",
-    title:  e.detail.value.title,
-    content:  e.detail.value.content,
-    typeSub: id,
-    pics: ""
-  }),
-  method: "POST",
-  header: {
-      'Content-Type': "application/x-www-form-urlencoded"
-  },
-  success: function(res) {
-    if(res.data.code=="1"){
-     that.toast1Tap()
-    }
-    console.log(res.data)
-  }
-})
+    wx.request({
+      url: 'http://172.21.101.175:11000/uplus/activity',
+      data: util.json2Form({
+        userId: "1",
+        title: e.detail.value.title,
+        content: e.detail.value.content,
+        typeSub: id,
+        pics: ""
+      }),
+      method: "POST",
+      header: {
+        'Content-Type': "application/x-www-form-urlencoded"
+      },
+      success: function (res) {
+        if (res.data.code == "1") {
+          that.toast1Tap()
+        }
+        console.log(res.data)
+      }
+    })
 
   },
   formReset: function () {
     console.log('form发生了reset事件')
   },
-  toast1Change: function(){
+  toast1Change: function () {
     this.setData({
       toast1Hidden: true
     })
   },
-  toast1Tap: function(){
-     this.setData({
+  toast1Tap: function () {
+    this.setData({
       toast1Hidden: false
     })
   }
